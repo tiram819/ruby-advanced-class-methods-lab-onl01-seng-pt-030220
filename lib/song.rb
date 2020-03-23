46fc214480f8d9755a50ecca8fd5cba8a6d7aeb9
Tabassum Hassan <tiram819@gmail.com>
2020-03-23 03:14:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -54,6 +54,19 @@ class Song
     song.artist_name = song_array[0]
     song
   end
+  
+  def self.create_from_filename(filename)
+    result = self.new_from_filename(filename)
+    song = self.create
+    song.name = result.name
+    song.artist_name = result.artist_name
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+end
     
   
   
