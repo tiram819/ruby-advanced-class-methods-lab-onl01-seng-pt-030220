5ffc5b2c73cc90ec235a39a6d502bc0d5df5781b
Tabassum Hassan <tiram819@gmail.com>
2020-03-23 03:12:40 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -45,6 +45,15 @@ class Song
     song = self.all.sort_by {|song| song.name}
     song
   end
+  
+   def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
     
   
   
